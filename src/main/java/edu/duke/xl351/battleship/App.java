20f74f80962e21046cd5181392f7473701d64106
Xiangzhou Luo <xl351@duke.edu>
2021-02-04 11:11:02 +0800

    goal1-task9

--- src/main/java/edu/duke/xl351/battleship/App.java
+++ src/main/java/edu/duke/xl351/battleship/App.java
@@ -3,12 +3,68 @@
  */
 package edu.duke.xl351.battleship;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+  final Board<Character> theBoard;
+  final BoardTextView view;
+  final BufferedReader inputReader;
+  final PrintStream out;
+  
+  /**
+   * Constructs a app with the borad, input source reader and ouput printstream
+   * @param theBoard is the {@link Board}.
+   * @param view is the text view of the board to display.
+   * @param input source reader and ouput printstream are the remaining two parameters
+   */
+
+  public App(Board<Character> theBoard, Reader inputSource, PrintStream out) {
+    this.theBoard = theBoard;
+    this.view = new BoardTextView(theBoard);
+    this.inputReader = new BufferedReader(inputSource);
+    this.out = out;
+  }
+
+
+  public Placement readPlacement(String prompt) throws IOException {
+    out.println(prompt);
+    String s = inputReader.readLine();
+    return new Placement(s);
+  }
+
+
+  public void doOnePlacement() throws IOException{
+    
+    Placement p = readPlacement("Where would you like to put your ship?");
+    Ship<Character> s = new BasicShip(p.getWhere());
+    theBoard.tryAddShip(s);
+    out.println(view.displayMyOwnBoard());
+  }
+
+  
+ 
+  public static void main(String[] args) throws IOException{
+    Board<Character> b = new BattleShipBoard<Character>(10,20);
+    
+    App app = new App(b,new InputStreamReader(System.in),System.out);
+    app.doOnePlacement();
+  }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
